21220640bfc8a21b65db4ed7a0a08c62d9c39bec
Sagar Kumar <pandeysagar7991@gmail.com>
2026-05-01 17:05:29 +0530

    databse

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,8 +12,8 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 
 /* POST /auth/register */
