1aadd8df2f55b7a48f7ef9495ba8d0cae3335d3c
Sagar Kumar <pandeysagar7991@gmail.com>
2026-05-02 19:36:23 +0530

    fix service worker manifest

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,10 +16,10 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", () => {
-//     // navigator.serviceWorker.register("/service-worker.js")
-//       .then(() => console.log("SW registered"))
-//       .catch(err => console.log("SW error:", err));
-//   });
-// }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(() => console.log("SW registered"))
+      .catch(err => console.log("SW error:", err));
+  });
+}
