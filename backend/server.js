11fd86f4dcf4a44a0e429e4ee50dd6722dd8eac7
Sagar Kumar <pandeysagar7991@gmail.com>
2026-05-02 17:19:51 +0530

    port updated

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ import connectToDatabase from './src/db/db.js';
 
 connectToDatabase();
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
     console.log('Server is running on http://localhost:3000');
 })
